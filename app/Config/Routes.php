dad2237c9bec9885bcad03b114c1c4818e99a7b4
Vicente <vicentefmm2019@gmail.com>
2024-02-06 08:53:52 -0300

    refactor: organização de rotas

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,8 +5,8 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/usuarios', 'Users::index');
-$routes->post('/login', 'Users::login');
-$routes->post('/usuarios', 'Users::create');
+$routes->get('/usuarios', 'Users::listUser');
+$routes->post('/login', 'Login::login');
+$routes->post('/cadastrar-usuario', 'Users::create');
 $routes->delete('/usuarios/(:num)', 'Users::delete/$1');
 $routes->patch('/usuarios/update/(:num)', 'Users::update/$1');
